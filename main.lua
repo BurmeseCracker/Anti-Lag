90890ef6fd4fe6a884b69587e5d8941379fb71be
Bhone Thit Htoo <112844311+BurmeseCracker@users.noreply.github.com>
2026-09-01 16:58:27 +0900

    Add function to get and execute GitHub files
    Implement a function to fetch and execute Lua files from GitHub.

---
+++ main.lua
@@ -0,0 +1,27 @@
+-- main.lua
+local GithubBase = "https://raw.githubusercontent.com/BurmeseCracker/RBX/refs/heads/main/"
+
+local function getGitHubFile(fileName)
+	local fileUrl = GithubBase .. fileName
+	
+	local success, content = pcall(function()
+		return game:HttpGet(fileUrl)
+	end)
+	
+	if not success or not content or content == "404: Not Found" or content:find("404") then
+		error("\n[Loader Error]: Failed to download '" .. fileName .. "'\nChecked URL: " .. fileUrl)
+	end
+	
+	local executableCode, compileError = loadstring(content)
+	if not executableCode then
+		error("\n[Syntax Error] Failed to compile code inside '" .. fileName .. "': " .. tostring(compileError))
+	end
+	
+	return executableCode()
+end
+
+local callPing = getGitHubFile("ping.lua")
+
+
+InitializeGui(AnimationDatabase, CoreHookFunction)
+print("[Success]: Ecosystem running perfectly!")
